bb0b30aa209bdfd8f23806b893153994707e5748
Mateus Moessa <mateus.alves2@estudante.ifms.edu.br>
2021-09-24 15:21:45 -0400

    +imagens

--- App.tsx
+++ App.tsx
@@ -1,14 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <Text>OI</Text>
+        
+        <Image source={require("./src/assets/arrow-left.png")}></Image>
+        <Image source={require("./src/assets/ei_search.png")}></Image>
+        <Image source={require("./src/assets/share.png")}></Image>
 
       </View>
+      <Text>OI</Text>
       <Text>Hello world</Text>
       <StatusBar style="auto" />
     </View>
@@ -21,7 +25,7 @@ const styles = StyleSheet.create({
   },
 
   header: {
-      height: 93,
+      minHeight: 93,
       backgroundColor: "#4369B0",
       paddingTop: 40,
 
