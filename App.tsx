ec4055f377af0b8a37162a016e1e1b88bc01c629
Mateus Moessa <mateus.alves2@estudante.ifms.edu.br>
2021-09-24 15:42:39 -0400

    fim do header

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput } from 'react-native';
 
 export default function App() {
   return (
@@ -8,7 +8,12 @@ export default function App() {
       <View style={styles.header}>
         
         <Image source={require("./src/assets/arrow-left.png")}></Image>
-        <Image source={require("./src/assets/ei_search.png")}></Image>
+        <View style={styles.inputTextView} >
+          <Image source={require("./src/assets/ei_search.png")}></Image>
+          <TextInput placeholder="Search" placeholderTextColor="#F5FFFF" style={styles.TextInput}>
+            
+          </TextInput>
+        </View>
         <Image source={require("./src/assets/share.png")}></Image>
 
       </View>
@@ -21,13 +26,31 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-
+    flex: 1,
   },
 
   header: {
       minHeight: 93,
       backgroundColor: "#4369B0",
       paddingTop: 40,
+      flexDirection: "row",
+      justifyContent: "space-between",
+      paddingHorizontal: 30,
+  },
 
-  }
+  inputTextView: {
+    flex: 3,
+    backgroundColor: "#2C4877",
+    height: 40,
+    marginHorizontal: 10,
+    alignItems: "center",
+    borderRadius: 28,
+    flexDirection: 'row',
+  },
+  TextInput: {
+    color: "#F5FFFF",
+    //maxWidth: 200,
+    flex: 1,
+    paddingRight: 10,
+  },
 });
