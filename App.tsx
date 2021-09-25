f81e91278c5908de8c1cc9ed5a25cea17d414dca
Mateus Moessa <mateus.alves2@estudante.ifms.edu.br>
2021-09-24 21:14:38 -0400

    Revert "Voltouuuuuuuuuuuuuuuuu"
    This reverts commit fa81621972760dbeca2b47eba864e58a0d91a1a0.

--- App.tsx
+++ App.tsx
@@ -2,13 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, Image, TextInput } from 'react-native';
 import Home from './src/Pages/Home';
-import Routes from './src/routes';
 
 
 export default function App() {
   return (
-      <View style={{flex: 1}}>
-      <Routes />
+    <View>
+      <Home />
       </View>
   );
 }
