573f6c424fc02590ccf5b9b6cda63bb2f1f2a985
Mateus Moessa <mateus.alves2@estudante.ifms.edu.br>
2021-10-03 22:54:36 -0400

    Cabeçalho do Post

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -8,6 +8,8 @@ import { stylesLink, stylesLinkImportantes, styles } from "./styles";
 import { NativeStackNavigatorProps } from "@react-navigation/native-stack/lib/typescript/src/types";
 import { StackNavigatorParamList } from "../../../types";
 
+import Post from '../Post';
+
 const LinkItem = (props: any)=>{
     return(
       <Text style={stylesLink.text}>{props.name}</Text> 
@@ -50,14 +52,16 @@ const Home = () => {
         <Image source={require("../../assets/share.png")}></Image>
 
       </View>
+      
       <LinksImportantes />
       <StatusBar style="auto" />
       <View>
-        <View style={styles.conteudoFacebook}>
+      <Post />
+        {/*<View style={styles.conteudoFacebook}>
           <TouchableOpacity style={styles.meubotao} onPress={irParaTelaLogin}>
               <Text style={styles.meubotaoTexto}>Botão</Text>
           </TouchableOpacity>
-        </View>
+        </View>*/}
       </View>
     </View>
     );   
